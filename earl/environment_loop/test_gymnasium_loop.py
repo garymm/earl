663ccafabb51558d200b01433ec08be87685e39e
Gary Miguel <garymm@garymm.org>
2025-02-06 01:21:33 +0000

    move mulity device test to its own file and target

--- earl/environment_loop/test_gymnasium_loop.py
+++ earl/environment_loop/test_gymnasium_loop.py
@@ -1,13 +1,8 @@
 import dataclasses
-import logging
-import time
-import typing
 
-import gymnasium.core
 import gymnax
 import gymnax.environments.spaces
 import jax
-import numpy as np
 import pytest
 from gymnasium.envs.classic_control.cartpole import CartPoleEnv
 from gymnasium.envs.classic_control.pendulum import PendulumEnv
@@ -189,57 +184,3 @@ def test_observe_cycle():
   loop.run(agent_state, num_cycles, steps_per_cycle)
   for _, v in metric_writer.scalars.items():
     assert v.get("ran", False)
-
-
-class SleepEnv(gymnasium.core.Env[np.int64, np.int64]):
-  def __init__(self, sleep_secs: float):
-    self.sleep_secs = sleep_secs
-    self.action_space = gymnasium.spaces.Discrete(2)
-    self.observation_space = gymnasium.spaces.Discrete(1)
-
-  def reset(
-    self, *, seed: int | None = None, options: dict[str, typing.Any] | None = None
-  ) -> tuple[np.int64, dict[str, typing.Any]]:
-    super().reset(seed=seed, options=options)
-    return np.int64(0), {}
-
-  def step(
-    self, action: np.int64
-  ) -> tuple[np.int64, typing.SupportsFloat, bool, bool, dict[str, typing.Any]]:
-    time.sleep(self.sleep_secs)
-    return np.int64(0), 0.0, False, False, {}
-
-
-def test_inference_update_different_devices(caplog):
-  # This test can run on two CPU devices using
-  # XLA_FLAGS="--xla_force_host_platform_device_count=2"
-  # but I can't figure out how to make that flag take effect
-  # only for this test before the test starts when using pytest.
-  cpu_devices = jax.devices("cpu")
-  if len(cpu_devices) >= 2:
-    devices = cpu_devices[:2]
-  else:
-    try:
-      devices = (cpu_devices[0], jax.devices("gpu")[0])
-    except RuntimeError:
-      pytest.skip("requires at least 2 devices")
-
-  num_envs = 2
-  env = SleepEnv(sleep_secs=0.1)
-  env_info = env_info_from_gymnasium(env, num_envs)
-  key_gen = keygen(jax.random.PRNGKey(0))
-  agent = RandomAgent(env_info.action_space.sample, 1)
-  metric_writer = MemoryWriter()
-
-  loop = GymnasiumLoop(
-    env, agent, num_envs, next(key_gen), metric_writer=metric_writer, devices=devices
-  )
-  num_cycles = 2
-  steps_per_cycle = 10
-  agent_state = agent.new_state(None, env_info, jax.random.PRNGKey(0))
-  # the default agent_state has nets=None.
-  # We set it to an array to check for use-after-donation bugs.
-  agent_state = dataclasses.replace(agent_state, nets=jax.numpy.ones((1,)))
-  caplog.set_level(logging.WARNING)
-  loop.run(agent_state, num_cycles, steps_per_cycle)
-  assert "inference is much slower than update" in caplog.text
